532651339cf6a18d93462bb3f65f847fb99ecbb2
johnworker <78348941+johnworker@users.noreply.github.com>
2022-08-27 20:51:37 +0800

    射擊時擺射擊姿勢

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -9,10 +9,12 @@ public class ThirdPersonShooterController : MonoBehaviour
     public GameObject crossGameObj;
 
     private StarterAssetsInputs _sai;
+    private Animator _anim;
     // Start is called before the first frame update
     void Start()
     {
         this._sai = this.GetComponent<StarterAssetsInputs>();
+        this._anim = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,11 +24,13 @@ public class ThirdPersonShooterController : MonoBehaviour
         {
             this.shootCamGameObj.SetActive(true);
             this.crossGameObj.SetActive(true);
+            this._anim.SetLayerWeight(1, Mathf.Lerp(this._anim.GetLayerWeight(1), 1f, Time.deltaTime * 10));
         }
         else
         {
             this.shootCamGameObj.SetActive(false);
             this.crossGameObj.SetActive(false);
+            this._anim.SetLayerWeight(1, Mathf.Lerp(this._anim.GetLayerWeight(1), 0f, Time.deltaTime * 10));
         }
     }
 }
