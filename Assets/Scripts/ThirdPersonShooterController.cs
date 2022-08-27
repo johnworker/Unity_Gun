1dfdbd5a20e4d2295641e5eca585c76c435a480e
johnworker <78348941+johnworker@users.noreply.github.com>
2022-08-27 22:33:17 +0800

    瞄準自動定位準心

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -20,11 +20,37 @@ public class ThirdPersonShooterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 先設為沒打到物件
+        Vector3 aimWorldPos = Vector3.zero;
+
+        #region 子彈射線判斷
+        /// <summary>
+        /// 新增射線做判斷
+        /// </summary>
+        Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+        if(Physics.Raycast(ray, out RaycastHit rh, 999f))
+        {
+            aimWorldPos = rh.point;
+        }
+        else
+        {
+            Debug.Log("none");
+        }
+        #endregion
+
+        /// <summary>
+        /// 瞄準
+        /// </summary>
         if (this._sai.aim)
         {
             this.shootCamGameObj.SetActive(true);
             this.crossGameObj.SetActive(true);
             this._anim.SetLayerWeight(1, Mathf.Lerp(this._anim.GetLayerWeight(1), 1f, Time.deltaTime * 10));
+
+            // 計算完後用normalized取得單位向量
+            Vector3 aimDirection = (aimWorldPos - transform.position).normalized;
+            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 50);
+
         }
         else
         {
