e15e057451da8fb090131cfe486060bbc8093804
johnworker <78348941+johnworker@users.noreply.github.com>
2022-08-28 17:59:02 +0800

    喪屍腳本調整

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -6,11 +6,15 @@ using UnityEngine.AI;
 public class Zombie : MonoBehaviour
 {
     public Transform target;
+    public List<Rigidbody> rigidList;
+    public Animator anim;
+
     private NavMeshAgent _name;
     // Start is called before the first frame update
     void Start()
     {
         this._name = this.GetComponent<NavMeshAgent>();
+        this.anim.PlayInFixedTime("Walk", 0, Random.Range(0.0f, 1.0f));
     }
 
     // Update is called once per frame
@@ -18,7 +22,17 @@ public class Zombie : MonoBehaviour
     {
         if (this._name.enabled)
         {
+            this._name.SetDestination(this.target.position);
+        }
+    }
 
+    public void kill()
+    {
+        this._name.enabled = false;
+        this.anim.enabled = false;
+        foreach (Rigidbody r in this.rigidList)
+        {
+            r.isKinematic = false;
         }
     }
 }
