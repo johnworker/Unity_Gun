b792b9a9981aae866994977dad3dae2e8946f632
johnworker <78348941+johnworker@users.noreply.github.com>
2022-08-28 16:48:24 +0800

    喪屍目標是主角

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
+    public Transform target;
+    private NavMeshAgent _name;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this._name = this.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (this._name.enabled)
+        {
+
+        }
     }
 }
